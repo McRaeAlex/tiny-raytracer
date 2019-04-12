00901cd09109c42ca41e3cc2083c3458dfc94b49
mcraealex <mcraealex16@gmail.com>
2019-04-11 22:39:39 -0700

    finished step 4

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,20 @@ use std::f32;
 
 use math::*;
 
+struct Light {
+    position: [f32; 3],
+    intensity: f32,
+}
+
+impl Light {
+    fn new(position: [f32; 3], intensity: f32) -> Light {
+        Light {
+            position,
+            intensity,
+        }
+    }
+}
+
 #[derive(Copy, Clone)]
 struct Material {
     diffuse_color: [u8; 3],
@@ -82,7 +96,12 @@ fn scene_intersect(
     closest_sphere_dist < 1000.0
 }
 
-fn cast_ray(origin: &[f32; 3], dir: &[f32; 3], spheres: &Vec<Sphere>) -> [u8; 3] {
+fn cast_ray(
+    origin: &[f32; 3],
+    dir: &[f32; 3],
+    spheres: &Vec<Sphere>,
+    lights: &Vec<Light>,
+) -> [u8; 3] {
     let mut point = [0.0, 0.0, 0.0];
     let mut n = [0.0, 0.0, 0.0];
     let mut m = Material::new([0, 0, 0]);
@@ -90,10 +109,28 @@ fn cast_ray(origin: &[f32; 3], dir: &[f32; 3], spheres: &Vec<Sphere>) -> [u8; 3]
     if !scene_intersect(origin, dir, spheres, &mut point, &mut n, &mut m) {
         return [0, 51, 0]; // background color
     }
-    return m.diffuse_color; // color of sphere
+
+    let mut diffuse_light_intesity = 0.0;
+    for light in lights {
+        let light_direction = normalize(&subtract(&light.position, &point));
+        diffuse_light_intesity += light.intensity * max(0.0, dot_prod(&light_direction, &n));
+    }
+
+    let mut f32_color = scalar_mult(
+        &[
+            m.diffuse_color[0] as f32,
+            m.diffuse_color[1] as f32,
+            m.diffuse_color[2] as f32,
+        ],
+        1.0 / 255.0,
+    );
+    f32_color = scalar_mult(&f32_color, diffuse_light_intesity);
+    f32_color = scalar_mult(&f32_color, 255.0);
+    let u8_version = convert_to_u8(f32_color);
+    return u8_version; // color of sphere
 }
 
-fn render(spheres: &Vec<Sphere>) {
+fn render(spheres: &Vec<Sphere>, lights: &Vec<Light>) {
     let width = 1024;
     let height = 768;
     let fov = f32::consts::PI / 2.0;
@@ -110,18 +147,20 @@ fn render(spheres: &Vec<Sphere>) {
         let i = (2.0 * (x_f + 0.5) / width_f - 1.0) * screen_width * width_f / height_f;
         let j = -(2.0 * (y_f + 0.5) / height_f - 1.0) * screen_width;
         let dir = normalize(&[i, j, -1.0]);
-        *pixel = image::Rgb(cast_ray(&[0.0, 0.0, 0.0], &dir, spheres));
+        *pixel = image::Rgb(cast_ray(&[0.0, 0.0, 0.0], &dir, spheres, lights));
     }
 
-    imgbuf.save("step3.png").expect("Could not write image");
+    imgbuf.save("step4.png").expect("Could not write image");
 }
 
 fn main() {
     let s = vec![
-        Sphere::new([-3.0, 0.0, -16.0], 2.0, Material::new([255, 255, 0])),
+        Sphere::new([-3.0, 0.0, -16.0], 2.0, Material::new([102, 102, 76])),
         Sphere::new([-1.0, -1.5, -12.0], 2.0, Material::new([76, 25, 25])),
         Sphere::new([1.5, -0.5, -18.0], 3.0, Material::new([76, 25, 25])),
-        Sphere::new([7.0, 5.0, -18.0], 4.0, Material::new([255, 255, 0])),
+        Sphere::new([7.0, 5.0, -18.0], 4.0, Material::new([102, 102, 76])),
     ];
-    render(&s);
+
+    let l = vec![Light::new([20.0, 20.0, 20.0], 1.5)];
+    render(&s, &l);
 }
--- src/math.rs
+++ src/math.rs
@@ -12,10 +12,25 @@ pub fn scalar_mult(u: &[f32; 3], t: f32) -> [f32; 3] {
     [u[0] * t, u[1] * t, u[2] * t]
 }
 
+pub fn scalar_mult_u8(u: &[u8; 3], t: f32) -> [f32; 3] {
+    [u[0] as f32 * t, u[1] as f32 * t, u[2] as f32 * t]
+}
+
 pub fn add(u: &[f32; 3], v: &[f32; 3]) -> [f32; 3] {
     [u[0] + v[0], u[1] + v[1], u[2] + v[2]]
 }
 
 pub fn subtract(u: &[f32; 3], v: &[f32; 3]) -> [f32; 3] {
     [u[0] - v[0], u[1] - v[1], u[2] - v[2]]
+}
+
+pub fn max(v1: f32, v2: f32) -> f32 {
+    if v2 > v1 {
+        return v2;
+    }
+    return v1;
+}
+
+pub fn convert_to_u8(v: [f32; 3]) -> [u8; 3] {
+    [v[0] as u8, v[1] as u8, v[2] as u8]
 }
